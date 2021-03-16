1ba168271ad0d5055ee21a472e2d83cccb3505b1
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-03-16 10:51:20 -0300

    fix number input

--- js-calc/src/App.js
+++ js-calc/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const boardNumbers = [
   ['7', '8', '9'],
@@ -28,28 +28,32 @@ const handleNumberInputs = (
   displayString,
   setDisplay,
   number,
-  evalArray,
+  evalString,
   setEval,
 ) => {
-  const data = displayString;
-  console.log('array display', data);
-  console.log('array eval', evalArray);
-
   if (displayString === '0') {
+    console.log('0');
     setEval(number);
     return number;
   }
-  if (displayString.match(/[0-9]\w+/g) === null) {
-    setEval([...evalArray, number]);
+  if (displayString.match(/[+-/*]/g) !== null) {
+    console.log('entrou no match');
+    console.log(displayString);
+
+    setEval(evalString + number);
+    // setDisplay(data);
+    // typeof display === 'string' && setEval([...evalString, display]);
     return number;
   }
-  setEval([...evalArray, number]);
-  // setDisplay(data);
-  // typeof display === 'string' && setEval([...evalArray, display]);
+  console.log('saiu do number');
+  console.log(displayString);
+  console.log(number);
+
+  setEval(evalString + number);
   return displayString + number;
 };
 
-function RenderNumbers(displayString, setDisplay, evalArray, setEval) {
+function RenderNumbers(displayString, setDisplay, evalString, setEval) {
   return (
     <ul>
       {boardNumbers.map((row, i) => (
@@ -65,7 +69,7 @@ function RenderNumbers(displayString, setDisplay, evalArray, setEval) {
                       displayString,
                       setDisplay,
                       e.target.value,
-                      evalArray,
+                      evalString,
                       setEval,
                     ),
                   )
@@ -81,24 +85,24 @@ function RenderNumbers(displayString, setDisplay, evalArray, setEval) {
   );
 }
 
-const handleInputSpecials = (displayString, special, evalArray, setEval) => {
+const handleInputSpecials = (displayString, special, evalString, setEval) => {
   console.log(special);
   if (special === 'C') {
-    setEval([]);
-    return ['0'];
+    setEval('0');
+    return '0';
   }
   if (displayString !== 'string' && displayString !== special) {
-    setEval([...evalArray, special]);
+    setEval(evalString + special);
     return special;
   }
   // typeof displayString !== 'string' &&
   //   displayString !== special &&
-  //   setEval([...evalArray, displayString]);
+  //   setEval([...evalString, displayString]);
   // return special;
   // : [display + special];
 };
 
-function RenderSpecials(displayString, setDisplay, evalArray, setEval) {
+function RenderSpecials(displayString, setDisplay, evalString, setEval) {
   return (
     <ul>
       {boardSpecials.map((row, i) => (
@@ -113,7 +117,7 @@ function RenderSpecials(displayString, setDisplay, evalArray, setEval) {
                     handleInputSpecials(
                       displayString,
                       e.target.value,
-                      evalArray,
+                      evalString,
                       setEval,
                     ),
                   )
@@ -131,14 +135,14 @@ function RenderSpecials(displayString, setDisplay, evalArray, setEval) {
 
 function JavaScriptCalculator() {
   const [displayString, setDisplay] = useState('0');
-  const [evalArray, setEval] = useState(['0']);
+  const [evalString, setEval] = useState('0');
   // const [change, setChange] = useState(false);
   // useEffect(() => {
   //   setEval(...display)
   // }, [])
   // useEffect(() => {
-  //   setEval([...evalArray, display]);
-  //   console.log(evalArray);
+  //   setEval([...evalString, display]);
+  //   console.log(evalString);
   // }, [display]);
   return (
     <div className="App">
@@ -149,11 +153,11 @@ function JavaScriptCalculator() {
         </p>
       </header>
       <p>EVAL</p>
-      <p>{evalArray}</p>
+      <p>{evalString}</p>
       <p>DISPLAY</p>
       <p id="display">{displayString}</p>
-      {RenderNumbers(displayString, setDisplay, evalArray, setEval)}
-      {RenderSpecials(displayString, setDisplay, evalArray, setEval)}
+      {RenderNumbers(displayString, setDisplay, evalString, setEval)}
+      {RenderSpecials(displayString, setDisplay, evalString, setEval)}
     </div>
   );
 }
