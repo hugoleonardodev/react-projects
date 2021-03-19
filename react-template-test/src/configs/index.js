2b098809495039fa197a9a4368389500b36924e9
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-03-18 23:18:05 -0300

    React Template with Tests

---
+++ react-template-test/src/configs/index.js
@@ -0,0 +1,11 @@
+import renderWithRouter from './renderWithRouter';
+import serviceWorker from './serviceWorker';
+import setupTests from './setupTests';
+
+const configs = {
+  renderWithRouter,
+  serviceWorker,
+  setupTests,
+};
+
+export default configs;
