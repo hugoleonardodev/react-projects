daf595b0aa5efc6df8120113ed22117436512c2f
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-04-25 03:03:33 -0300

    all structure and routes done

--- blog-crud/src/App.js
+++ blog-crud/src/App.js
@@ -5,6 +5,9 @@ import Home from './pages/Home';
 import Update from './pages/Update';
 import Create from './pages/Create';
 import List from './pages/List';
+import Read from './pages/Read';
+import Favorites from './pages/Favorites';
+import About from './pages/About';
 
 class App extends Component {
   render() {
@@ -13,7 +16,10 @@ class App extends Component {
         <Route exact path="/" component={Home} />
         <Route exact path="/create" component={Create} />
         <Route exact path="/list" component={List} />
+        <Route exact path="/read/:id" component={Read} />
         <Route exact path="/update/:id" component={Update} />
+        <Route exact path="/favorites" component={Favorites} />
+        <Route exact path="/about" component={About} />
       </Switch>
     );
   }
